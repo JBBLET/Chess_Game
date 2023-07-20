75cd01aa3304dc49927f828ddcfa0a0663ae8c5b
Jean-Baptiste BLET <blet.jb@gmail.com>
2023-07-20 16:31:40 +0900

    added the pawn class

--- piecetype.py
+++ piecetype.py
@@ -4,24 +4,53 @@ import numpy as np
 
 class Pawn(Piece):
 
-    def __init__(self,x,y,couleur,board):
-        super().__init__(x,y,couleur,board)
-        self.__firstmove = True
-        if self.__couleur == 'black':
-            self.__direction = np.array([(0,-1)])
-        else : 
-            self.__direction = np.array([(0,1)])
-        self.__value = 1
+    def __init__(self,white):
+        super().__init__(white)
+        self.__already_moved = False
 
-    def setMoves(self,Board): 
-        moves = []
-        if self.__firstmove:
-            moves.append(self.__position + self.__direction)
-            moves.append(self.__position + 2*self.__direction)
+    def get_already_moved(self):
+        return self.__already_moved
+    
+    def set_already_moved(self,already_moved):
+        self.__already_moved = already_moved
+
+    def can_move(self, board, start, end):
+        #We can't move to where a same color piece is
+        if end.get_piece().is_white() == self.is_white():
+            return False
+        
+        dif_x , dif_y = abs(start.get_pos_X()-end.get_pos_X()), abs(start.get_pos_Y()-end.get_pos_Y())
+        if dif_x == 0 and dif_y == 1 and end.get_piece() == None:
+            return True
+        elif dif_x == 1 and dif_y == 1 and end.get_piece().is_white() == not(self.is_white()):
+            return True
+        elif dif_x == 0 and dif_y == 2:
+            return(self.is_valid_first_move(board,start,end))
         else:
-            moves.append(self.__position + self.__direction)
-        if Board[self.__position[0]+self.__direction[1],self.__postion[1]+1].getCouleur != self.__couleur :
-            moves.append((self.__position[0]+self.__direction[1],self.__postion[1]+1))
-        if Board[self.__position[0]+self.__direction[1],self.__postion[1]-1].getCouleur != self.__couleur :
-            moves.append((self.__position[0]+self.__direction[1],self.__postion[1]-1))
-        self.__moves= moves
+            return False
+        
+    def is_valid_first_move(self,board,start,end):
+        if self.__already_moved:
+            return False
+        middle_spot = board.get_spot(end.get_pos_X(),(start.get_pos_Y()+end.get_pos_Y())//2)
+        if middle_spot.get_piece()==None:
+            return True
+
+class Bishops(Piece):
+    def __init__(self,white):
+        super().__init__(white)
+
+    def can_move(self,board,start,end):
+        #We can't move to where a same color piece is
+        if end.get_piece().is_white() == self.is_white():
+            return False
+        
+        dif_x , dif_y = abs(start.get_pos_X()-end.get_pos_X()), abs(start.get_pos_Y()-end.get_pos_Y())
+        
+        if dif_x!=dif_y:
+            return False
+        else:
+            return(self.no_piece_in_between(board,start,end))
+        
+    def no_piece_in_between(self,board,star,end):
+        
