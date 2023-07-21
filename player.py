f670866a617aac1236b627632bf33bb4e01bf10c
Jean-Baptiste BLET <blet.jb@gmail.com>
2023-07-21 15:27:59 +0900

    created player classes

---
+++ player.py
@@ -0,0 +1,24 @@
+class Player :
+    def __init__(self):
+        self.__white_side = None
+        self.__is_human = None
+    
+    def get_is_white_side(self):
+        return self.__white_side
+    
+    def get_is_human(self):
+        return self.__is_human
+    
+class Human_player(Player):
+    
+    def __init__(self,white_side):
+        super().__init__()
+        self.__is_human = True
+        self.__white_side = white_side
+
+class Human_player(Player):
+    
+    def __init__(self,white_side):
+        super().__init__()
+        self.__is_human = False
+        self.__white_side = white_side
