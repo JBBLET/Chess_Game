4ea8d815487a203ae767f552270ba64627a4bf7c
Jean-Baptiste BLET <blet.jb@gmail.com>
2023-07-21 16:12:37 +0900

    created access private arguments methods

--- game.py
+++ game.py
@@ -1,3 +1,15 @@
+import board
+import player
+import move
+
 class Game:
-    def __init__(self):
-        self.__player = []
+
+    def __init__(self,player_1,player_2):
+        self.__player = [player_1, player_2]
+        self__board = board.Board()
+        self.__current_turn = player_1 if player_1.get_is_white() else player_2
+        self.__status = "Active"
+        self.__moves_played = []
+
+    def make_move(self, player, move):
+        selected_piece = move.get_move()
