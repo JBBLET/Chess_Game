c10ee2eff75f86b909e9e1ca8bf7a6dfb7d8f45b
Jean-Baptiste BLET <blet.jb@gmail.com>
2023-07-21 14:35:55 +0900

    added comments

--- board.py
+++ board.py
@@ -5,15 +5,18 @@ import piecetype
 class Board:
     
     def __init(self):
+        #Initialisation
         self.__board = [[]*7]*7
         self.reset_board()
 
     def reset_board(self):
         #Reset the board
         for i in range(8):
+            #add the pawns
             self.__board[i][1] = spot(i, 1, pawn(True))
             self.__board[i][6] = spot(i, 1, pawn(False))
         
+        #Add the white pieces
         self.__board[0][0] = spot(0, 0, rook(True))
         self.__board[1][0] = spot(1, 0, knight(True))
         self.__board[2][0] = spot(2, 0, bishop(True))
@@ -23,6 +26,7 @@ class Board:
         self.__board[6][0] = spot(6, 0, knight(True))
         self.__board[7][0] = spot(7, 0, rook(True))
 
+        #Add the black pieces
         self.__board[0][7] = spot(0, 7, rook(False))
         self.__board[1][7] = spot(1, 7, knight(False))
         self.__board[2][7] = spot(2, 7, bishop(False))
@@ -32,11 +36,13 @@ class Board:
         self.__board[6][7] = spot(6, 7, knight(False))
         self.__board[7][7] = spot(7, 7, rook(False))
         
+        #Add the blank spaces
         for i in range(2,7,1):
             for j in range(8):
                 self.__board[i][j] = spot(i, j, None)
 
     def get_spot(self,x,y):
+        #Return the spot at the coordinates x,y
         try:
             return self.__board[x][y]
         except IndexError:
